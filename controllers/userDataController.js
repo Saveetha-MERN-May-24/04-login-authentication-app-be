698ec855c6077ec0d976a067fa371d56115d0208
Sethu Ravichandran <sethu@Sethus-MacBook-Air.local>
2024-05-16 01:23:24 +0530

    Added the entire code.

--- controllers/userDataController.js
+++ controllers/userDataController.js
@@ -1,1 +1,27 @@
-// This is the user data controller
+const userModel = require('../models/userModel')
+const jwt = require('jsonwebtoken')
+const JWT_TOKEN = 'thdofunymsfwbcifmdgsndifmfhsksik'
+
+const displayUserData = async(request, response) => {
+    const {token} = request.body
+
+    try
+    {
+        const loggedInUser = jwt.verify(token, JWT_TOKEN)
+        const loggedInUserEmail = loggedInUser.email
+        const authenticatedUser = await userModel.findOne({email : loggedInUserEmail})
+
+        if (authenticatedUser)
+            {
+                return response.status(200).json(authenticatedUser)
+            }
+        
+        response.status(400).send({message : 'Something went wrong, try again!'})
+    }
+    catch(error)
+    {
+        response.status(500).send({message : error.message})
+    }
+}
+
+module.exports = {displayUserData}
